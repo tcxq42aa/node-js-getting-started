7b1ddf5aa0158f2a6a6545613f59d1fcd9b839e5
charles <xiaoaojianghuxq@126.com>
2016-02-25 00:48:21 +0800

    update

---
+++ config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by charles on 16/2/25.
+ */
+module.exports = {
+    LC_APP_ID: 'ex9v5l84hwbsc4isq2h2o228uqvqnrj5dbl9khq6c5q2c5ob',
+    LC_APP_KEY: 'xytdfxpok3yqe41pdzaav0c4akuj3j07znos7y55jugeug4m',
+    LC_APP_MASTER_KEY: '602i2mx9sib2ffv5pnd5sjwosg3eno2ieaqs7np0hn2ea2js'
+};
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 'use strict';
 var AV = require('leanengine');
 
-var APP_ID = process.env.LC_APP_ID;
-var APP_KEY = process.env.LC_APP_KEY;
-var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
+var config = require('./config');
+var APP_ID = process.env.LC_APP_ID || config.LC_APP_ID;
+var APP_KEY = process.env.LC_APP_KEY || config.LC_APP_KEY;
+var MASTER_KEY = process.env.LC_APP_MASTER_KEY || config.LC_APP_MASTER_KEY;
 
 AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
@@ -13,7 +14,7 @@ var app = require('./app');
 
 // 端口一定要从环境变量 `LC_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
-var PORT = parseInt(process.env.LC_APP_PORT || 3000);
+var PORT = parseInt(process.env.LC_APP_PORT || 3001);
 app.listen(PORT, function () {
   console.log('Node app is running, port:', PORT);
 });
